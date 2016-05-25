7bebb62c4bc8bb49ed43c839888ef611fc48fc48
hdd <dangduonghung@gmail.com>
2016-05-25 10:40:47 +0800

    update

--- linkedin.py
+++ linkedin.py
@@ -12,6 +12,7 @@ import scrapy
 
 class LikedinItem(scrapy.Item):
     # define the fields for your item here like:
+    companyId = scrapy.Field()
     companyname = scrapy.Field()
     fullname = scrapy.Field()
     firstname = scrapy.Field()
@@ -25,6 +26,9 @@ class MySpider(InitSpider):
     start_urls = ['https://www.linkedin.com/vsearch/p?f_CC=3530383&trk=rr_connectedness']
     ids = []
     base_url = 'https://www.linkedin.com'
+    custom_settings = {
+        "FEED_EXPORT_FIELDS" : ["companyId", "companyname", "firstname" ,"lastname","fullname","profileurl"]
+    }
     #extractor = SgmlLinkExtractor()
 
     #rules = (
@@ -45,7 +49,7 @@ class MySpider(InitSpider):
     def login(self, response):
         """Generate a login request."""
         return FormRequest.from_response(response,
-                    formdata={'session_key': 'xxx@gmail.com', 'session_password': 'xxx'},
+                    formdata={'session_key': 'xxx@gmail.com', 'session_password': 'yyy'},
                     callback=self.check_login_response)
 
     def check_login_response(self, response):
@@ -63,23 +67,24 @@ class MySpider(InitSpider):
             for id in self.ids:
                 url = 'https://www.linkedin.com/vsearch/p?f_CC=%s&trk=rr_connectedness' % id
                 print "url=======%s" % url
-                yield scrapy.Request(url, self.parsess)
+                yield scrapy.Request(url, self.parsess, meta={'company_id': id})
 
             # yield Request('https://www.linkedin.com/vsearch/p?f_CC=208401&trk=rr_connectedness', self.parsess)
             # yield Request('https://www.linkedin.com/vsearch/p?f_CC=3530383&trk=rr_connectedness', self.parsess)
             self.initialized()
 
         else:
-            self.log("Bad times :(")
+            self.log("Incorrect username/password!")
             # Something went wrong, we couldn't log in, so nothing happens.
 
     def parsess(self, response):
         self.log("====parse_item")
         print response.url
+        print "+++COMPANY_ID***", response.meta['company_id']
         # hxs = HtmlXPathSelector(response)
         # print hxs.xpath('//div[@id="resulfts-container"]').extract()
-        with open('log_fpt.html', 'a') as f:
-		    f.write(response.body)
+        # with open('log_4999332.html', 'a') as f:
+		#     f.write(response.body)
 
 
         # self.driver.get(response.url)
@@ -104,7 +109,14 @@ class MySpider(InitSpider):
         code = soup.find('code', id="voltron_srp_main-content").contents[0].replace(r'\u002d', '-')
         json_code = json.loads(code)
 
-        # json_code['content']['page']['voltron_unified_search_json']['search']['baseData']
+        resultPagination = None
+        if 'resultPagination' in json_code['content']['page']['voltron_unified_search_json']['search']['baseData']:
+            resultPagination = json_code['content']['page']['voltron_unified_search_json']['search']['baseData']['resultPagination']
+
+        # print resultPagination['nextPage']['pageURL']
+        if (resultPagination is not None ) and ('nextPage' in resultPagination) and ('pageURL' in resultPagination['nextPage']) and (resultPagination['nextPage']['pageURL'].strip()):
+            nextPageUrl =  self.__to_absolute_url(self.base_url, resultPagination['nextPage']['pageURL'].strip())
+            yield Request(nextPageUrl, self.parsess, meta={'company_id': response.meta['company_id']})
 
         employees = json_code['content']['page']['voltron_unified_search_json']['search']['results']
 
@@ -119,9 +131,18 @@ class MySpider(InitSpider):
             firstName = person["firstName"] if 'firstName' in person else ""
             lastName = person["lastName"] if 'lastName' in person else ""
             fullName = person["fmt_name"] if 'fmt_name' in person else ""
-            company = BeautifulSoup(person['snippets'][0]["heading"]).text
-            item = LikedinItem(companyname=company, fullname=fullName, firstname=firstName, lastname=lastName, profileurl=profileUrl)
+            # company = BeautifulSoup(person['snippets'][0]["heading"]).text
+            company = ""
+            if ('snippets' in person) and (len(person['snippets']) > 0) and ('heading' in person['snippets'][0]):
+                company = self.remove_tags(person['snippets'][0]["heading"])
+            else:
+                print "company empty=============", repr(person['snippets'])
+
+            company_id = response.meta['company_id']
+
+            item = LikedinItem(companyId=company_id, companyname=company, fullname=fullName, firstname=firstName, lastname=lastName, profileurl=profileUrl)
             profiles.append(item)
+            yield item
             # company_list.append("%s\t%s\t%s\t%s\n" % (name, fmt_industry, fmt_size, fmt_location))
 
         # for profile in profiles:
@@ -155,8 +176,8 @@ class MySpider(InitSpider):
         #
         #     item = LikedinItem(companyname=companie, fullname=fullName, firstname=firstName, lastname=lastName, profileurl=profileUrl)
         #     items.append(item)
-        print len(profiles)
-        return profiles
+        # print len(profiles)
+        # yield profiles
 
     def get_employee_link(employes):
         profiles=[]
@@ -166,12 +187,15 @@ class MySpider(InitSpider):
         return profiles , len(profiles)
 
     def remove_tags(p, p2):
-        print "remove_tags p++++++++++++++++", repr(p), "remove_tags p2###################", repr(p2)
+        # print "remove_tags p++++++++++++++++", repr(p), "remove_tags p2###################", repr(p2)
         # p=str(p)
 
+        soup = BeautifulSoup(p2, 'html.parser')
+        print "++++++++++++++++ soup removed tags: ", soup.get_text()
+        return soup.get_text()
         # u'<!--{"content":{....."status":"ok"}-->'
-        p3 = repr(p2)
-        return p3[6: -4]
+        # p3 = repr(p2)
+        # return p3[6: -4]
 
     def __to_absolute_url(self, base_url, link):
         '''
